abbd6bf734c99205dcb6b9c60bbc23b7bffedb6d
Marcos Vinicius <marcao.adsistema@gmail.com>
2023-03-12 13:47:58 -0300

    feat: content tweet layout

--- src/main.tsx
+++ src/main.tsx
@@ -49,7 +49,24 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <button className="new-tweet">Tweet</button>
       </aside>
 
-      <div className="content">content</div>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Phosphor.Sparkle />
+          </div>
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/m4rcaoferole.png" alt="Marcos Vinicius" />
+              <textarea id="tweet" placeholder="What's happening" />
+            </label>
+            <button type="submit">Tweet</button>
+
+            <div className="separator" />
+          </form>
+        </main>
+      </div>
     </div>
   </React.StrictMode>,
 );
